aff238d635566d30364467ad395818b85d11e0af
falafeless <nwelchr@gmail.com>
2018-02-16 08:24:11 -0800

    Create stack.rb

---
+++ w1d5/stack.rb
@@ -0,0 +1,23 @@
+class Stack
+
+  attr_accessor :stack
+
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    stack.push(el)
+  end
+
+  def remove
+    stack.pop
+  end
+
+  def show
+    stack
+  end
+end
+
+
+wow_stack = Stack.new
